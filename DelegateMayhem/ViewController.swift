2156d639d7a3a6ca013df08f69479d2ca178f20c
mallorymcginty <mallory.mcginty@mu.edu>
2017-03-02 16:52:23 -0600

    Initial Commit

---
+++ DelegateMayhem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DelegateMayhem
+//
+//  Created by Ivor D. Addo, PhD on 3/2/17.
+//  Copyright © 2017 Mallory McGinty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
